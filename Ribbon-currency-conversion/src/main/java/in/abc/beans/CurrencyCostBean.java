c142e3ad720bdc795236cf1fdb1b8d5e9df8819e
Prateek <prateekbanra7@gmail.com>
2025-08-26 13:05:56 +0530

    first commit

---
+++ Ribbon-currency-conversion/src/main/java/in/abc/beans/CurrencyCostBean.java
@@ -0,0 +1,13 @@
+package in.abc.beans;
+
+import lombok.Data;
+
+@Data
+public class CurrencyCostBean {
+
+	private String currencyFrom;
+	private String currencyTo;
+	private Double totalCurrencyAmt;
+	private Integer port;
+
+}
